c0c007befc68978cf1ef649fff65904174ce8f0e
Alexey Zarubin <alexeygzarubin@gmail.com>
2016-02-02 21:57:18 -0800

    Changed the output string to 'Alexey Zarubin'.

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ public class HelloOlga
  {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Alexey");
+        System.out.println("Alexey Zarubin");
     }
 }

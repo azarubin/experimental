1e4e452c20cb2e724121eb62d73a773ba23ce363
Alexey <azarubin@google.com>
2016-02-12 11:02:43 -0800

    Added a sum(a, b) function

--- main.java
+++ main.java
@@ -1,8 +1,15 @@
-public class HelloAlexey
+public class Main
  {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, Olga and Alexey");
 	System.out.println("One more line here");
+	int z = 1;
+	int x = 2;
+	System.out.println("Sum of " + z + " and " + x + " equals " + sum(z, x) + ".");
+    }
+    
+    public static int sum(int a, int b) {
+    	return a + b;
     }
 }

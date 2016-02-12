ae8b6a18be54654246fb132d38d1acd6d5059bd3
Olga Kozlova <okozlova@ebay.com>
2016-02-12 11:14:37 -0800

    Added a multiplication method.

--- main.java
+++ main.java
@@ -7,9 +7,15 @@ public class Main
 	int z = 1;
 	int x = 2;
 	System.out.println("Sum of " + z + " and " + x + " equals " + sum(z, x) + ".");
+	System.out.println("2 * 2 = "+ mult(2, 2));
+
     }
     
     public static int sum(int a, int b) {
     	return a + b;
+    }
+
+    public static int mult(int a, int b){
+	return a * b;
     }
 }

4eb3ee760e0449b857a326fe9ecd3eda4101f4f0
Alexey <azarubin@google.com>
2016-02-12 11:48:18 -0800

    Updated main.java

--- main.java
+++ main.java
@@ -7,6 +7,8 @@ public class Main
 	int z = 1;
 	int x = 2;
 	System.out.println("Sum of " + z + " and " + x + " equals " + sum(z, x) + ".");
+	System.out.println("Sum of " + x + " and " + z + " equals " + sum(x, z) + ".");
+	System.out.println("Sum of " + x + " and " + x + " equals " + sum(x, x) + ".");
     }
     
     public static int sum(int a, int b) {

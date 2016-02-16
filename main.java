8f5a3a27a2205a01ee92dfe32b63d4be8d01d376
Alexey <azarubin@google.com>
2016-02-15 19:59:17 -0800

    b-sum a-sum

--- main.java
+++ main.java
@@ -4,16 +4,11 @@ public class Main
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, Olga and Alexey");
 	System.out.println("One more line here");
-	int z = 1;
-	int x = 2;
-	int y = 3;
 	int a = 4;
-	System.out.println("Sum of " + z + " and " + x + " equals " + sum(z, x) + ".");
-	System.out.println("2 * 2 = " + mult(2, 2));
-	System.out.println("Sum of " + y + " and " + y + " equals " + sum(y, y) + ".");
-	System.out.println("Sum of " + a + " and " + a + " equals " + sum(a, a) + ".");
+	int b = 5;
 	System.out.println("3 * 3 = " + mult(3, 3));
-	System.out.println("Sum of " + x + " and " + x + " equals " + sum(x, x) + ".");
+	System.out.println("Sum of " + a + " and " + a + " equals " + sum(a, a) + ".");
+	System.out.println("Sum of " + b + " and " + b + " equals " + sum(b, b) + ".");
     }
     
     public static int sum(int a, int b) {

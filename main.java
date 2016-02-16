f19c91fbe1c3a748372c134a4cbc25fd96fd650e
Alexey <azarubin@google.com>
2016-02-15 19:43:39 -0800

    A-sum

--- main.java
+++ main.java
@@ -7,9 +7,11 @@ public class Main
 	int z = 1;
 	int x = 2;
 	int y = 3;
+	int a = 4;
 	System.out.println("Sum of " + z + " and " + x + " equals " + sum(z, x) + ".");
 	System.out.println("2 * 2 = " + mult(2, 2));
 	System.out.println("Sum of " + y + " and " + y + " equals " + sum(y, y) + ".");
+	System.out.println("Sum of " + a + " and " + a + " equals " + sum(a, a) + ".");
     }
     
     public static int sum(int a, int b) {
